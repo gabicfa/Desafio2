69821a129e40db05b7d7eabe1840e525c88d0c6f
Gabriela Almeida <gabrielaalmeida@Gabrielas-MacBook-Pro.local>
2017-05-06 19:05:26 -0300

    Parte extra

--- grafo.hxx
+++ grafo.hxx
@@ -8,7 +8,7 @@ class Grafo
 	int n_academicos;
 
 public:
-  std::vector<int> *conexoes;
+	std::vector<int> *conexoes;
 	std::vector<std::string> *conexoes_nome;
 	Grafo(int n_academicos);
 	void adicionarConexoes(int academico1, int academico2);
